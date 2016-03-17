5f4da23ac5f6f19be5d5f034d950ceaacb8a2149
Ashwini08 <ashwini.gavate@ishutechllp.com>
2016-03-17 15:35:20 +0530

    first commit

---
+++ JunitDemo/src/test/java/Test99GuruLogin.java
@@ -0,0 +1,44 @@
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test99GuruLogin {
+
+   static WebDriver driver;
+	Guru99Login objLogin;
+	Guru99HomePage objHomePage;
+	
+	@BeforeClass
+	public static void setup(){
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://demo.guru99.com/V4/");
+	}
+
+	/**
+	 * This test case will login in http://demo.guru99.com/V4/
+	 * Verify login page title as guru99 bank
+	 * Login to application
+	 * Verify the home page using Dashboard message
+	 */
+	@Test
+	public void test_Home_Page_Appear_Correct(){
+		//Create Login Page object
+	objLogin = new Guru99Login(driver);
+	//Verify login page title
+	String loginPageTitle = objLogin.getLoginTitle();
+	Assert.assertTrue(loginPageTitle.toLowerCase().contains("guru99 bank"));
+	//login to application
+	objLogin.loginToGuru99("mngr31165", "bUharAq");
+	// go the next page
+	objHomePage = new Guru99HomePage(driver);
+	//Verify home page
+	Assert.assertTrue(objHomePage.getHomePageDashboardUserName().toLowerCase().contains("manger id : mngr31165"));
+	}
+	
+}
